5d1660d47f231b930c22026bc95d990e528972ec
Miles Bronson <mbronson@bandwidth.com>
2022-11-02 17:23:05 -0400

    minor fixes

--- audio.py
+++ audio.py
@@ -4,14 +4,20 @@ import sqlite3
 import uuid
 import flask
 import wave
+ 
+DEEPGRAM_AUDIO_ROOT = pathlib.Path(__file__).resolve().parent
+UPLOAD_FOLDER = DEEPGRAM_AUDIO_ROOT/'database'/'uploads'
+DATABASE_FILENAME = DEEPGRAM_AUDIO_ROOT/'database'/'deepgramAudio.db'
 
 audio = flask.Flask(__name__)
+
+
 
 def dict_factory(cursor, row):
     return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
 
 def get_db():
-    db_filename = audio.config['DATABASE_FILENAME']
+    db_filename = DATABASE_FILENAME
     conn = sqlite3.connect(str(db_filename))
     conn.row_factory = dict_factory
     return conn
@@ -21,43 +27,39 @@ def get_db():
 def post_file():
     """Post Raw Audio File"""
     print("test")
-    #print(flask.request.files)
+    print(flask.request.files)
     #print(flask.request.form.__dict__)
-    print(flask.request.data)
-    file = flask.request.form['onlyFile']
-
-    print("test2")
+    #print(flask.request.data)
+    file = flask.request.files['file']
     filename = file.filename
-    print("filename:", filename)
     stem = uuid.uuid4().hex
     suffix = pathlib.Path(filename).suffix
     uuid_basename = f"{stem}{suffix}"
-    path = audio.config["UPLOAD_FOLDER"]/uuid_basename
+    path = UPLOAD_FOLDER/uuid_basename
     file.save(path)
     duration_seconds = 0
-    with wave.open(file) as file_wav:
+    with wave.open(filename, mode='rb') as file_wav:
         duration_seconds = file_wav.getnframes() / file_wav.getframerate()
     connection = get_db()
     connection.execute(
         "INSERT INTO audio"
         "(filename, filepath, duration) "
         "VALUES (?, ?, ?)",
-        (filename, path, duration_seconds, )
+        (filename, uuid_basename, int(duration_seconds), )
     )
     return flask.jsonify(), 201
 
 
 @audio.route("/download")
-def get_file():
+def get_filename():
     connection = get_db()
     filename = flask.request.args.get('name')
     db_output = connection.execute(
-        "SELECT filepath "
+        "SELECT * "
         "FROM audio "
         "WHERE filename = ?",
         (filename, )
     )
-    files = db_output.fetchall()
     files = db_output.fetchall()
     return_data = []
     for file in files:
@@ -75,13 +77,12 @@ def get_file():
 @audio.route("/list")
 def get_list():   
     connection = get_db()
-    filename = flask.request.args.get('maxduration')
+    maxduration = flask.request.args.get('maxduration')
     db_output = connection.execute(
-        "SELECT filepath "
+        "SELECT * "
         "FROM audio "
-        "WHERE size < ? "
-        "OR",
-        (filename, )
+        "WHERE duration <= ? ",
+        (maxduration, )
     )
     files = db_output.fetchall()
     return_data = []
@@ -101,9 +102,10 @@ def get_metadata():
     connection = get_db()
     filename = flask.request.args.get('name')
     db_output = connection.execute(
-        "SELECT filename, size, created "
+        "SELECT * "
         "FROM audio "
-        "WHERE filename = ?",
+        "WHERE filename = ? "
+        "ORDER BY duration",
         (filename, )
     )
     files = db_output.fetchall()
@@ -111,7 +113,7 @@ def get_metadata():
     for file in files:
         file_data = {
             "filename": file["filename"],
-            "size": file["size"],
+            "size": file["duration"],
             "created": file["created"]
         }
         return_data.append(file_data)
@@ -120,5 +122,3 @@ def get_metadata():
     }
     return context
 
-if __name__ == '__main__':
-    audio.run(host='0.0.0.0', port=80)
